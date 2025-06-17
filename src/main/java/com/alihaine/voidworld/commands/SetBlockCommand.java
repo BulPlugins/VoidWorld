6f0f7d34cf096bffdaa28df79a827e5226e9ef65
AliHaine <alitr57120@gmail.com>
2025-06-17 20:26:40 +0200

    Use the new command system made from the BulMultiverse version 0.14

--- src/main/java/com/alihaine/voidworld/commands/SetBlockCommand.java
+++ src/main/java/com/alihaine/voidworld/commands/SetBlockCommand.java
@@ -1,19 +1,17 @@
 package com.alihaine.voidworld.commands;
 
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
 import com.alihaine.bulmultiverse.command.BaseBmvCommand;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 @CommandAlias("bmv|bulmv|bulmultiverse")
-@Description("Create a block at your position")
 @CommandPermission("bulmultiverse.setblock")
 public class SetBlockCommand extends BaseBmvCommand {
 
     @Subcommand("setblock")
+    @Description("Create a block at your position")
+    @Syntax("/bmv setblock")
     public void onSetBlock(Player player) {
         player.getLocation().getBlock().setType(Material.BEDROCK);
     }
--- src/main/java/com/alihaine/voidworld/commands/SetVoidCommand.java
+++ src/main/java/com/alihaine/voidworld/commands/SetVoidCommand.java
@@ -1,19 +1,17 @@
 package com.alihaine.voidworld.commands;
 
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Description;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
 import com.alihaine.bulmultiverse.command.BaseBmvCommand;
 import com.alihaine.voidworld.chunks.ChunkVoidMaker;
 import org.bukkit.entity.Player;
 
 @CommandAlias("bmv|bulmv|bulmultiverse")
-@Description("The chunk at your position will be void")
 @CommandPermission("bulmultiverse.setvoid")
 public class SetVoidCommand extends BaseBmvCommand {
 
     @Subcommand("setvoid")
+    @Description("The chunk at your position will be void")
+    @Syntax("/bmv setvoid")
     public void onSetVoid(Player player) {
         ChunkVoidMaker.MakeChunkVoid(player.getLocation().getChunk());
     }
