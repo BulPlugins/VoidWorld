b9fc30dce3bad78961eb21c6732c6f17a6429833
AliHaine <alitr57120@gmail.com>
2025-07-30 00:01:15 +0200

    Update to work with bmv 1.1

--- src/main/java/com/alihaine/voidworld/commands/SetBlockCommand.java
+++ src/main/java/com/alihaine/voidworld/commands/SetBlockCommand.java
@@ -1,13 +1,14 @@
 package com.alihaine.voidworld.commands;
 
+import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import com.alihaine.bulmultiverse.command.BaseBmvCommand;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
-@CommandAlias("bmv|bulmv|bulmultiverse")
+@CommandAlias(BaseBmvCommand.commandRootAlias)
 @CommandPermission("bulmultiverse.setblock")
-public class SetBlockCommand extends BaseBmvCommand {
+public class SetBlockCommand extends BaseCommand {
 
     @Subcommand("setblock")
     @Description("Create a block at your position")
--- src/main/java/com/alihaine/voidworld/commands/SetVoidCommand.java
+++ src/main/java/com/alihaine/voidworld/commands/SetVoidCommand.java
@@ -1,13 +1,14 @@
 package com.alihaine.voidworld.commands;
 
+import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import com.alihaine.bulmultiverse.command.BaseBmvCommand;
 import com.alihaine.voidworld.chunks.ChunkVoidMaker;
 import org.bukkit.entity.Player;
 
-@CommandAlias("bmv|bulmv|bulmultiverse")
+@CommandAlias(BaseBmvCommand.commandRootAlias)
 @CommandPermission("bulmultiverse.setvoid")
-public class SetVoidCommand extends BaseBmvCommand {
+public class SetVoidCommand extends BaseCommand {
 
     @Subcommand("setvoid")
     @Description("The chunk at your position will be void")
